9825d90bfb5d594db25801f6a361a8f1c86cac61
Ghost <ghost@Ghosts-MacBook-Pro.local>
2022-04-04 11:30:43 -0400

    added test python file from mac

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  4 11:23:30 2022
+
+@author: ghost
+"""
+
+import pandas as pd
+
+df = pd.read_csv('2022_ds_glassdoor_jobs.csv')
