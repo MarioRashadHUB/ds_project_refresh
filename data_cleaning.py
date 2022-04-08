ce85d4d2aa3a4bdb66f89de7f8b7939b5ab41b53
Mario <blackiviagic@gmail.com>
2022-04-08 14:04:19 -0400

    updated data cleaning script

--- data_cleaning.py
+++ data_cleaning.py
@@ -15,8 +15,11 @@ df = pd.read_csv('2022_ds_glassdoor_jobs.csv')
 # age of company
 # parsing of job description (python, ect)
 
+df['hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
+df['employer_provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided salary:' in x.lower() else 0)
+
 df = df[df['Salary Estimate'] != '-1' ]
 salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
 minus_Kd = salary.apply(lambda x: x.replace('K', '').replace('$',''))
-df['hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
-df['employer_provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided salary:' in x.lower() else 0)
+
+min_hr = minus_Kd.apply(lambda x: x.lower().replace('per hour','').replace('employer provided salary','') )
